45b3de7b927c2e23ce369343f6a910b28fca2ead
Raghav Ramesh <raghavramesh94@gmail.com>
2015-02-01 17:48:43 +0800

    feat: add test methods for testing double and back quotes, double and single quotes and single and back quotes -back and single quote test failed ; fix: swap expected and actual parameters;

--- test/sg/edu/nus/comp/cs4218/impl/QuoteParserTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/QuoteParserTest.java
@@ -10,31 +10,57 @@ import org.junit.Test;
 import sg.edu.nus.comp.cs4218.impl.QuoteParser;
 
 public class QuoteParserTest {
-  @Test
-  public void parseWithDoubleQuoteOnly() {
-    List<String> tokens = QuoteParser.parse("I hate \"Java\" and \"S c a l a\"");
-    List<String> expected = Arrays.asList("I", "hate", "\"Java\"", "and", "\"S c a l a\"");
-    assertEquals(tokens, expected);
-   }
-  
-  @Test
-  public void parseWithSingleQuoteOnly() {
-    List<String> tokens = QuoteParser.parse("I hate 'Java' and 'S c a l a'");
-    List<String> expected = Arrays.asList("I", "hate", "'Java'", "and", "'S c a l a'");
-    assertEquals(tokens, expected);
-   }
-  
-  @Test
-  public void parseWithBackQuoteOnly() {
-    List<String> tokens = QuoteParser.parse("I hate `Java` and `S c a l a`");
-    List<String> expected = Arrays.asList("I", "hate", "`Java`", "and", "`S c a l a`");
-    assertEquals(tokens, expected);
-  }
-  
-  @Test
-  public void parseWithMixQuotes() {
-    List<String> tokens = QuoteParser.parse("echo \"this is space: `echo \" \"`\"");
-    List<String> expected = Arrays.asList("echo", "\"this is space: `echo \" \"`\"");
-    assertEquals(tokens, expected);
-  }
+	@Test
+	public void parseWithDoubleQuoteOnly() {
+		List<String> tokens = QuoteParser
+				.parse("I hate \"Java\" and \"S c a l a\"");
+		List<String> expected = Arrays.asList("I", "hate", "\"Java\"", "and",
+				"\"S c a l a\"");
+		assertEquals(expected, tokens);
+	}
+
+	@Test
+	public void parseWithSingleQuoteOnly() {
+		List<String> tokens = QuoteParser
+				.parse("I hate 'Java' and 'S c a l a'");
+		List<String> expected = Arrays.asList("I", "hate", "'Java'", "and",
+				"'S c a l a'");
+		assertEquals(expected, tokens);
+	}
+
+	@Test
+	public void parseWithBackQuoteOnly() {
+		List<String> tokens = QuoteParser
+				.parse("I hate `Java` and `S c a l a`");
+		List<String> expected = Arrays.asList("I", "hate", "`Java`", "and",
+				"`S c a l a`");
+		assertEquals(expected, tokens);
+	}
+
+	@Test
+	public void parseWithDoubleAndBackQuotes() {
+		List<String> tokens = QuoteParser
+				.parse("echo \"this is space: `echo \" \"`\"");
+		List<String> expected = Arrays.asList("echo",
+				"\"this is space: `echo \" \"`\"");
+		assertEquals(expected, tokens);
+	}
+
+	@Test
+	public void parseWithDoubleAndSingleQuotes() {
+		List<String> tokens = QuoteParser
+				.parse("echo \"this is space: 'echo ' ''\"");
+		List<String> expected = Arrays.asList("echo",
+				"\"this is space: 'echo ' ''\"");
+		assertEquals(expected, tokens);
+	}
+
+	@Test
+	public void parseWithBackAndSingleQuotes() {
+		List<String> tokens = QuoteParser
+				.parse("echo 'this is space: 'echo \" \"''");
+		List<String> expected = Arrays.asList("echo",
+				"'this is space: 'echo \" \"''");
+		assertEquals(expected, tokens);
+	}
 }
