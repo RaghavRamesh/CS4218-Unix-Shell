255ce6a824875b390c4163c109b677b3f921aed8
Shubham <1shubham@gmail.com>
2015-02-08 15:05:14 +0800

    see #2: completed head functionality. Added exception messages.

--- src/sg/edu/nus/comp/cs4218/impl/app/HeadApp.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/HeadApp.java
@@ -2,6 +2,7 @@ package sg.edu.nus.comp.cs4218.impl.app;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,72 +14,84 @@ import java.io.PrintWriter;
 import sg.edu.nus.comp.cs4218.Application;
 import sg.edu.nus.comp.cs4218.Consts;
 import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
-import sg.edu.nus.comp.cs4218.exception.CatException;
-import sg.edu.nus.comp.cs4218.exception.CdException;
+import sg.edu.nus.comp.cs4218.exception.HeadException;
 
 public class HeadApp implements Application {
-
-	public HeadApp() {
-		// TODO Auto-generated constructor stub
-	}
 
 	@Override
 	public void run(String[] args, InputStream stdin, OutputStream stdout) throws AbstractApplicationException {
 
-		// TODO: check if all readers (stdin and filereaders) are properly closed
-		// TODO: when reading from file, throw exception if file doesnt exist
-
-		// different examples
-		// head "sdfsdf" -- only stdin present --1
-		// head filename.sdfs -- only arg[0] present --2
-		// head -n 15 "afsdfsd" -- arg[0] and stdin present --3
-		// head -n 23 filename.sdfsdf -- arg[0] and arg[1] present --4
-		// conclusion --
+		// 4 different cases:
+		// --1-- head "sdfsdf" -- only stdin needed
+		// --2-- head filename.sdfs -- only arg[0] needed
+		// --3-- head -n 15 "afsdfsd" -- arg[0] and stdin needed
+		// --4-- head -n 23 filename.sdfsdf -- arg[0] and arg[1] needed
 
 		if (args == null) {
-			throw new CdException(Consts.Messages.ARG_NOT_NULL);// TODO
+			throw new HeadException(Consts.Messages.ARG_NOT_NULL);// TODO: check if this is needed
 		}
 
 		if (stdout == null) {
-			throw new CdException(Consts.Messages.OUT_STR_NOT_NULL);// TODO
+			throw new HeadException(Consts.Messages.OUT_STR_NOT_NULL);
 		}
 
-		String requiredDirectory;
 		BufferedReader reader = null;
 		PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(stdout)));
-		int numOfLines = 10;
+		int numOfLines = 10; // by default, read only 10 lines
 
-		// if (args.length == 1) {
-		// numOfLines = Integer.parseInt(args[0]); // TODO: check if valid?
-		// }
-
-		if (args.length == 0) { // 1
+		if (args.length == 0) { // --1--
 
 			if (stdin == null) {
-				throw new CatException("input stream is null"); // TODO
+				throw new HeadException(Consts.Messages.NO_INPUT_FILE_OR_STDIN);
 			}
 			reader = new BufferedReader(new InputStreamReader(stdin));
 			writeToPrintStream(writer, numOfLines, reader);
-		} else if (args.length == 1) { // 2,3
 
-			if (stdin == null) { // 2
-				reader = new BufferedReader(new FileReader(args[0]));
-			} else { // 3
+		} else if (args.length == 1) {
 
+			try {
+				if (stdin == null) { // --2--
+					reader = new BufferedReader(new FileReader(args[0]));
+				} else { // --3--
+					numOfLines = Integer.parseInt(args[0]);
+					reader = new BufferedReader(new InputStreamReader(stdin));
+				}
+				writeToPrintStream(writer, numOfLines, reader);
+			} catch (FileNotFoundException e) {
+				throw new HeadException(e.getMessage());
+			} catch (NumberFormatException e) {
+				throw new HeadException(Consts.Messages.ILLEGAL_LINE_COUNT);
 			}
 
-		} else if (args.length == 2) { // 4
-
-		} else { // ERROR too many arguments
-
+		} else if (args.length == 2) { // --4--
+			try {
+				numOfLines = Integer.parseInt(args[0]);
+				reader = new BufferedReader(new FileReader(args[1]));
+				writeToPrintStream(writer, numOfLines, reader);
+			} catch (NumberFormatException e) {
+				throw new HeadException(Consts.Messages.ILLEGAL_LINE_COUNT);
+			} catch (FileNotFoundException e) { // TODO:
+				throw new HeadException(e.getMessage());
+			}
+		} else {
+			throw new HeadException(Consts.Messages.TOO_MANY_ARGUMENTS);
 		}
 
 	}
 
 	// protected for testing
-	protected void writeToPrintStream(PrintWriter writer, int numOfLines, final BufferedReader br) {
-		String line = null;
+	protected void writeToPrintStream(PrintWriter writer, int numOfLines, final BufferedReader br) throws AbstractApplicationException {
 
+		// TODO: see if double checking needed
+		if (writer == null)
+			throw new HeadException(Consts.Messages.OUT_STR_NOT_NULL);
+		if (br == null)
+			throw new HeadException(Consts.Messages.IN_STR_NOT_NULL);
+		if (numOfLines < 0)
+			throw new HeadException(Consts.Messages.ILLEGAL_LINE_COUNT);
+		;
+
+		String line = null;
 		try {
 			for (int i = 0; i < numOfLines; i++) {
 				if ((line = br.readLine()) != null) {
@@ -89,14 +102,10 @@ public class HeadApp implements Application {
 
 				writer.write("\n");
 				br.close();
-			}
-		} catch (IOException e) { // TODO:
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			if (writer != null) {
 				writer.close();
 			}
+		} catch (IOException e) {
+			throw new HeadException(e.getMessage());
 		}
 	}
 
