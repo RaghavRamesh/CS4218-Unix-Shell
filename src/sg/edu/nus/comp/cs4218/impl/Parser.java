baecbf82e32c860b380fdf42ae1d6d26fea45b43
Raghav Ramesh <raghavramesh94@gmail.com>
2015-02-17 17:32:07 +0800

    fix: merge commits

--- src/sg/edu/nus/comp/cs4218/impl/Parser.java
+++ src/sg/edu/nus/comp/cs4218/impl/Parser.java
@@ -15,18 +15,20 @@ public final class Parser {
 	static final Character DOUBLE_QUOTE = '"';
 	static final Character SINGLE_QUOTE = '\'';
 	static final Character BACK_QUOTE = '`';
-	
+
 	static final Character SEMICOLON = ';';
 	static final Character PIPE = '|';
 	static final Character IN_STREAM = '<';
 	static final Character OUT_STREAM = '>';
-	
-	static final List<Character> SPECIALS = Arrays.asList(SEMICOLON, PIPE, IN_STREAM, OUT_STREAM);
+
+	static final List<Character> SPECIALS = Arrays.asList(SEMICOLON, PIPE,
+			IN_STREAM, OUT_STREAM);
 
 	private Parser() {
 	}
 
-	public static List<String> parseCommandLine(String input) throws ShellException {
+	public static List<String> parseCommandLine(String input)
+			throws ShellException {
 		String trimmedInput = input.trim();
 		StringBuilder currentToken = new StringBuilder();
 		ArrayList<String> tokens = new ArrayList<String>();
@@ -40,7 +42,9 @@ public final class Parser {
 			}
 			currentToken.append(currentChar);
 			if (isQuote(currentChar)) {
-				if (quoteStack.isEmpty() || !quoteStack.peek().getCharacter().equals(currentChar)) {
+				if (quoteStack.isEmpty()
+						|| !quoteStack.peek().getCharacter()
+								.equals(currentChar)) {
 					quoteStack.push(new CharacterPosition(currentChar, i));
 				} else {
 					quoteStack.pop();
@@ -50,73 +54,69 @@ public final class Parser {
 					}
 				}
 			} else if (quoteStack.isEmpty() && SPECIALS.contains(currentChar)) {
-			  // Add the current token without the last character
-			  addNonEmptyToList(tokens, currentToken.substring(0, currentToken.length() - 1));
-			  // Add the last character
-			  addNonEmptyToList(tokens, currentChar.toString());
-			  currentToken = new StringBuilder();
+				// Add the current token without the last character
+				addNonEmptyToList(tokens,
+						currentToken.substring(0, currentToken.length() - 1));
+				// Add the last character
+				addNonEmptyToList(tokens, currentChar.toString());
+				currentToken = new StringBuilder();
 			}
 		}
 		if (!quoteStack.isEmpty()) {
-		  throw new ShellException(Consts.Messages.QUOTE_MISMATCH);
-		} 
+			throw new ShellException(Consts.Messages.QUOTE_MISMATCH);
+		}
 		addNonEmptyToList(tokens, currentToken.toString());
 		return tokens;
 	}
 
 	public static Boolean isQuoted(String input) {
-	  return isDoubleQuoted(input)
-	      || isSingleQuoted(input)
-	      || isBackQuoted(input);
+		return isDoubleQuoted(input) || isSingleQuoted(input)
+				|| isBackQuoted(input);
 	}
-	
-  public static Boolean isDoubleQuoted(String input) {
-    int length = input.length();
-    if (length < 2) {
-      return false;
-    }
-    return (input.charAt(0) == DOUBLE_QUOTE)
-        && (input.charAt(length - 1) == DOUBLE_QUOTE);
-  }
-	
+
+	public static Boolean isDoubleQuoted(String input) {
+		int length = input.length();
+		if (length < 2) {
+			return false;
+		}
+		return (input.charAt(0) == DOUBLE_QUOTE)
+				&& (input.charAt(length - 1) == DOUBLE_QUOTE);
+	}
+
 	public static Boolean isSingleQuoted(String input) {
-	  int length = input.length();
-    if (length < 2) {
-      return false;
-    }
-    return (input.charAt(0) == SINGLE_QUOTE)
-        && (input.charAt(length - 1) == SINGLE_QUOTE);
+		int length = input.length();
+		if (length < 2) {
+			return false;
+		}
+		return (input.charAt(0) == SINGLE_QUOTE)
+				&& (input.charAt(length - 1) == SINGLE_QUOTE);
 	}
-	
+
 	public static Boolean isBackQuoted(String input) {
-	  int length = input.length();
-	  if (length < 2) {
-	    return false;
-	  }
-	  return (input.charAt(0) == BACK_QUOTE)
-	      && (input.charAt(length - 1) == BACK_QUOTE);
+		int length = input.length();
+		if (length < 2) {
+			return false;
+		}
+		return (input.charAt(0) == BACK_QUOTE)
+				&& (input.charAt(length - 1) == BACK_QUOTE);
 	}
-	
+
 	public static Boolean isSemicolon(String input) {
-	  return input.length() == 1
-	      && input.charAt(0) == SEMICOLON;
+		return input.length() == 1 && input.charAt(0) == SEMICOLON;
 	}
-	
+
 	public static Boolean isPipe(String input) {
-	  return input.length() == 1
-	      && input.charAt(0) == PIPE;
+		return input.length() == 1 && input.charAt(0) == PIPE;
 	}
-	
+
 	public static Boolean isInStream(String input) {
-	  return input.length() == 1
-	      && input.charAt(0) == IN_STREAM;
+		return input.length() == 1 && input.charAt(0) == IN_STREAM;
 	}
-	
+
 	public static Boolean isOutStream(String input) {
-	  return input.length() == 1
-	      && input.charAt(0) == OUT_STREAM;
+		return input.length() == 1 && input.charAt(0) == OUT_STREAM;
 	}
-	 
+
 	private static Boolean addNonEmptyToList(List<String> list, String str) {
 		if (str.trim().equals("")) {
 			return false;
