5d04f8cd1e32c9cf98bb722c1c8140cec14ea01c
Shubham <1shubham@gmail.com>
2015-02-23 14:27:49 +1100

    see #2: added tail app

---
+++ src/sg/edu/nus/comp/cs4218/impl/app/TailApp.java
@@ -0,0 +1,133 @@
+package sg.edu.nus.comp.cs4218.impl.app;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+import sg.edu.nus.comp.cs4218.Application;
+import sg.edu.nus.comp.cs4218.Consts;
+import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
+import sg.edu.nus.comp.cs4218.exception.HeadException;
+import sg.edu.nus.comp.cs4218.exception.TailException;
+
+public class TailApp implements Application {
+
+	@Override
+	public void run(String[] args, InputStream stdin, OutputStream stdout) throws AbstractApplicationException {
+
+		// 4 different cases:
+		// --1-- head "sdfsdf" -- only stdin needed
+		// --2-- head filename.sdfs -- only arg[0] needed
+		// --3-- head -n 15 "afsdfsd" -- arg[0] and stdin needed
+		// --4-- head -n 23 filename.sdfsdf -- arg[0] and arg[1] needed
+
+		if (args == null) {
+			throw new HeadException(Consts.Messages.ARG_NOT_NULL);// TODO: check if this is needed
+		}
+
+		if (stdout == null) {
+			throw new HeadException(Consts.Messages.OUT_STR_NOT_NULL);
+		}
+
+		BufferedReader reader = null;
+		PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(stdout)));
+		int numOfLines = 10; // by default, read only 10 lines
+
+		if (args.length == 0) { // --1--
+
+			if (stdin == null) {
+				throw new HeadException(Consts.Messages.NO_INPUT_FILE_OR_STDIN);
+			}
+			reader = new BufferedReader(new InputStreamReader(stdin));
+			writeToPrintStream(writer, numOfLines, reader);
+
+		} else if (args.length == 1) {
+
+			try {
+				if (stdin == null) { // --2--
+					reader = new BufferedReader(new FileReader(args[0]));
+				} else { // --3--
+					numOfLines = Integer.parseInt(args[0]);
+					reader = new BufferedReader(new InputStreamReader(stdin));
+				}
+				writeToPrintStream(writer, numOfLines, reader);
+			} catch (FileNotFoundException e) {
+				throw new HeadException(Consts.Messages.FILE_DOES_NOT_EXIST);
+			} catch (NumberFormatException e) {
+				throw new HeadException(Consts.Messages.ILLEGAL_LINE_COUNT);
+			}
+
+		} else if (args.length == 2) { // --4--
+			try {
+				numOfLines = Integer.parseInt(args[0]);
+				reader = new BufferedReader(new FileReader(args[1]));
+				writeToPrintStream(writer, numOfLines, reader);
+			} catch (NumberFormatException e) {
+				throw new HeadException(Consts.Messages.ILLEGAL_LINE_COUNT);
+			} catch (FileNotFoundException e) {
+				throw new HeadException(Consts.Messages.FILE_DOES_NOT_EXIST);
+			}
+		} else {
+			throw new HeadException(Consts.Messages.TOO_MANY_ARGUMENTS);
+		}
+
+	}
+
+	private long getLineCount(BufferedReader br) throws AbstractApplicationException {
+		long numOfLines = 0;
+
+		try {
+			while (br.readLine() != null)
+				numOfLines++;
+		} catch (IOException e) {
+			throw new TailException(e.getMessage());
+		}
+		return numOfLines;
+	}
+
+	private void writeToPrintStream(PrintWriter writer, int numOfLinesToRead, final BufferedReader br) throws AbstractApplicationException {
+
+		// TODO: see if double checking needed
+		if (writer == null)
+			throw new HeadException(Consts.Messages.OUT_STR_NOT_NULL);
+		if (br == null)
+			throw new HeadException(Consts.Messages.IN_STR_NOT_NULL);
+		if (numOfLinesToRead < 0)
+			throw new HeadException(Consts.Messages.ILLEGAL_LINE_COUNT);
+
+		long lineCount = getLineCount(br);
+		long numOfLinesToNotRead = 0;
+		if (lineCount > numOfLinesToRead)
+			numOfLinesToNotRead = lineCount - numOfLinesToNotRead;
+
+		String line = null;
+		try {
+
+			for (long i = 0; i < numOfLinesToNotRead; i++)
+				br.readLine();
+
+			for (int i = 0; i < numOfLinesToRead; i++) {
+				if ((line = br.readLine()) != null) {
+					writer.write(line);
+				} else {
+					break;
+				}
+
+				writer.write("\n");
+			}
+
+			br.close();
+			writer.close();
+		} catch (IOException e) {
+			throw new HeadException(e.getMessage());
+		}
+	}
+
+}
